d317f1e38de329ed472c4836551606edb0e21cc3
molnarnoah <molnarnoahh@gmail.com>
2026-04-21 19:44:42 +0200

    Update BionicleQFNPalladium.java

--- addon/BionicleQFNPalladium.java
+++ addon/BionicleQFNPalladium.java
@@ -15,7 +15,6 @@ public class BionicleQFNPalladium {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         BionicleQFNPalladiumSounds.REGISTRY.register(bus);
         BionicleQFNPalladiumBlocks.REISTRY.register(bus);
-        
     }
 
 
